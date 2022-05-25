cd724ad4eb21f81a7756af42be37d948faaa8e3d
Raphael Lee <jhlee7125@gmail.com>
2022-05-25 22:55:26 +0900

    todoList.jsx - edit typo

--- react-basic/todo-list/src/components/todoList.jsx
+++ react-basic/todo-list/src/components/todoList.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import Todo from './todo';
 import TodoForm from './todoForm';
+import Todo from './todo';
 
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
@@ -27,9 +27,9 @@ const TodoList = () => {
   };
 
   const removeTodo = (id) => {
-    const removeArr = [...todos].filter((todo) => todo.id !== id);
+    const removedArr = [...todos].filter((todo) => todo.id !== id);
 
-    setTodos(removeArr);
+    setTodos(removedArr);
   };
 
   const completeTodo = (id) => {
@@ -44,7 +44,7 @@ const TodoList = () => {
 
   return (
     <>
-      <h1>What's the Plan for Today</h1>
+      <h1>What's the Plan for Today?</h1>
       <TodoForm onSubmit={addTodo} />
       <Todo
         todos={todos}
@@ -60,12 +60,12 @@ export default TodoList;
 
 /* 
 Note
-/^\s*$/.test(val) uses RegExp test method to test whether a string 'val' is empty or only contains spaces.
+/^\s*$/.test{val} uses RegExp test method to test whether a string 'val' is empty or only contains spaces.
 In docs:
 - The test() method executes a search for a match between a regular expression and a specified string.
-- Return true or false.
+- Return true or false
 
 Basically, it means that:
-- /^ ... $/ : matches a string from the beginning to the end
-- \s* : matches zero or more occurrences of white space characters
- */
+- /^...$/ : matches a string from the beginning to the end
+- \s* : matches zero or more occurences of white space characters
+*/
