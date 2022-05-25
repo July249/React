3a95d2706a55180912a3cb7876dbc7e52d1d509d
Raphael Lee <jhlee7125@gmail.com>
2022-05-25 22:28:41 +0900

    react-basic todo-list v1

---
+++ react-basic/todo-list/src/components/todoList.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import Todo from './todo';
+import TodoForm from './todoForm';
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+    console.log(todo, ...todos);
+  };
+
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    );
+  };
+
+  const removeTodo = (id) => {
+    const removeArr = [...todos].filter((todo) => todo.id !== id);
+
+    setTodos(removeArr);
+  };
+
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <>
+      <h1>What's the Plan for Today</h1>
+      <TodoForm onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
+    </>
+  );
+};
+
+export default TodoList;
+
+/* 
+Note
+/^\s*$/.test(val) uses RegExp test method to test whether a string 'val' is empty or only contains spaces.
+In docs:
+- The test() method executes a search for a match between a regular expression and a specified string.
+- Return true or false.
+
+Basically, it means that:
+- /^ ... $/ : matches a string from the beginning to the end
+- \s* : matches zero or more occurrences of white space characters
+ */
