80c7d34f5551705092c17ef04e109ff59cd7371e
jihyunglee <jhlee7125@gmail.com>
2022-05-30 14:27:36 +0900

    fetching api data and structure loading page

--- react-basic/tabs/src/app.jsx
+++ react-basic/tabs/src/app.jsx
@@ -4,6 +4,38 @@ import { FaAngleDoubleRight } from 'react-icons/fa';
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project';
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  // fetching api data
+  const fetchJobs = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const newJobs = await response.json();
+      setLoading(false);
+      setJobs(newJobs);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  // Loading page structure
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>Loading...</h1>
+      </section>
+    );
+  }
+
   return <h2>Tabs Project Set up</h2>;
 }
 
