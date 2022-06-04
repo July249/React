0971005aa4fd4d893dc442c43c36f26776c066a0
jihyunglee <jhlee7125@gmail.com>
2022-06-04 10:35:15 +0900

    slider setup

---
+++ react-basic/slider/src/app.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import { FiChevronRight, FiChevronLeft, FiChevronsRight } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
+function App() {
+  const [people, setPeople] = useState(data);
+  const [idx, setIdx] = useState(0);
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
+          // more stuff coming up
+
+          return (
+            <article key={id}>
+              <img className="person-img" src={image} alt={name} />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+        <button className="prev">
+          <FiChevronLeft />
+        </button>
+        <button className="next">
+          <FiChevronsRight />
+        </button>
+      </div>
+    </section>
+  );
+}
+
+export default App;
